4f883beb74b300f4ace2b0ca678f045db5646431
gquang06 <nguyengiaquang061@gmail.com>
2026-04-28 18:03:26 +0700

    add code for RTP-base (not tested yet)

--- RTP-base/receiver.py
+++ RTP-base/receiver.py
@@ -5,25 +5,60 @@ from utils import PacketHeader, compute_checksum
 
 
 def receiver(receiver_ip, receiver_port, window_size):
-    """TODO: Listen on socket and print received message to sys.stdout."""
+
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind((receiver_ip, receiver_port))
+
+    expected_seq_num = 0
+    isConnected = False
+    
+    while (isConnected == False):
+
+        pkt, address = s.recvfrom(2048)
+        pkt_header = PacketHeader(pkt[:16])
+
+        if (pkt_header.checksum == compute_checksum(pkt_header) 
+                and pkt_header.type == 0 
+                and pkt_header.seq_num == 0):
+            
+            start_ack = PacketHeader(type=3, seq_num=1, length=0)
+            start_ack.checksum = compute_checksum(start_ack)
+            s.sendto(bytes(start_ack), address)
+            expected_seq_num += 1
+            isConnected = True
+   
     while True:
+
         # Receive packet; address includes both IP and port
         pkt, address = s.recvfrom(2048)
-
-        # Extract header and payload
         pkt_header = PacketHeader(pkt[:16])
-        msg = pkt[16 : 16 + pkt_header.length]
 
-        # Verity checksum
-        pkt_checksum = pkt_header.checksum
-        pkt_header.checksum = 0
-        computed_checksum = compute_checksum(pkt_header / msg)
-        if pkt_checksum != computed_checksum:
-            print("checksums not match")
-        print(msg)
+        # Check for end packet
+        if pkt_header.type == 1:
 
+            end_ack = PacketHeader(type=3, seq_num=expected_seq_num+1, length=0)
+            end_ack.checksum = compute_checksum(end_ack)
+            s.sendto(bytes(end_ack), address)
+            print("Connection closed")
+            break
+
+        # send ACK for received packet if it is not corrupted and in order
+        if (pkt_header.checksum == compute_checksum(pkt_header) 
+                and pkt_header.type == 2 
+                and pkt_header.seq_num == expected_seq_num):
+            
+            msg = pkt[16 : 16 + pkt_header.length]
+            print(msg.decode("utf-8"), end="")
+            expected_seq_num += 1
+            ack_pkt = PacketHeader(type=3, seq_num=expected_seq_num, length=0)
+            ack_pkt.checksum = compute_checksum(ack_pkt)
+            s.sendto(bytes(ack_pkt), address)
+
+        # resend ACK for last in-order packet if received packet is out of order or corrupted
+        else:
+            ack_pkt = PacketHeader(type=3, seq_num=expected_seq_num, length=0)
+            ack_pkt.checksum = compute_checksum(ack_pkt)
+            s.sendto(bytes(ack_pkt), address)
 
 def main():
     parser = argparse.ArgumentParser()
--- RTP-base/sender.py
+++ RTP-base/sender.py
@@ -1,17 +1,92 @@
 import argparse
 import socket
+import sys
 
-from utils import PacketHeader, compute_checksum
+from utils import PacketHeader, compute_checksum  
 
+time_out = 0.500
 
 def sender(receiver_ip, receiver_port, window_size):
-    """TODO: Open socket and send message from sys.stdin."""
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    pkt_header = PacketHeader(type=2, seq_num=10, length=14)
-    pkt_header.checksum = compute_checksum(pkt_header / "Hello, world!\n")
-    pkt = pkt_header / "Hello, world!\n"
-    s.sendto(bytes(pkt), (receiver_ip, receiver_port))
+    # read input message
+    message = sys.stdin.read()
+    chunk_size = 1024
+    chunk_data = []
+    
+    # split input message into sized chunks
+    for i in range(0, len(message), chunk_size):
+        if i < len(message) - chunk_size:
+            chunk_data.append(message[i:i+chunk_size])
+        else:
+            chunk_data.append(message[i:len(message)])
 
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(time_out)
+
+    seq_num = 0
+    isConnected = False
+
+    while not isConnected:
+
+        pkt_header = PacketHeader(type=0, seq_num=seq_num, length=0)
+        pkt_header.checksum = compute_checksum(pkt_header)
+        s.sendto(bytes(pkt_header), (receiver_ip, receiver_port))
+        
+        try:
+            pkt, _ = s.recvfrom(2048)
+            pkt_header = PacketHeader(pkt[:16])
+            if pkt_header.checksum == compute_checksum(pkt_header) and pkt_header.type == 3 and pkt_header.seq_num == 1:
+                print("Connection established")
+                isConnected = True
+                seq_num = 1
+
+        except socket.timeout:
+            print("Connection timeout")
+
+    base = seq_num
+
+    while base <= len(chunk_data):
+    
+        for i in range(base, min(base + window_size, len(chunk_data) + 1)):
+
+            payload = chunk_data[i - 1]
+            pkt_header = PacketHeader(type=2, seq_num=i, length=len(payload))
+            pkt_header.checksum = compute_checksum(pkt_header / payload)
+            s.sendto(bytes(pkt_header / payload), (receiver_ip, receiver_port))
+        
+        try:
+            while True:
+                pkt, _ = s.recvfrom(2048)
+                pkt_header = PacketHeader(pkt[:16])
+                if (pkt_header.checksum == compute_checksum(pkt_header) 
+                        and pkt_header.type == 3
+                        and pkt_header.seq_num > base):
+                    
+                    base = pkt_header.seq_num
+
+        except socket.timeout:
+            print("ACK timeout, resending packets in window")
+
+    isEndAck = False
+
+    while not isEndAck:
+
+        # send end packet
+        pkt_header = PacketHeader(type=1, seq_num=base, length=0)
+        pkt_header.checksum = compute_checksum(pkt_header)
+        s.sendto(bytes(pkt_header), (receiver_ip, receiver_port))
+        
+        try:
+            pkt, _ = s.recvfrom(2048)
+            pkt_header = PacketHeader(pkt[:16])
+            if (pkt_header.checksum == compute_checksum(pkt_header) 
+                    and pkt_header.type == 3 
+                    and pkt_header.seq_num == base + 1):
+
+                print("Connection closed")
+                isEndAck = True
+
+        except socket.timeout:
+            print("Connection timeout, resending end packet")
 
 def main():
     parser = argparse.ArgumentParser()
